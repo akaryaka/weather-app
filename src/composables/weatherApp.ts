4bc4dab2626d29763e598de75d28bc72853851c4
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-29 17:45:17 +0600

    add async

--- src/composables/weatherApp.ts
+++ src/composables/weatherApp.ts
@@ -1,13 +1,16 @@
 import axios from "axios";
-import { values } from "../constants";
+import { data } from "../constants";
+import { watch } from "vue";
 
-export  function getWeather() {
-  if (values.value.city.trim().length < 3) {
-    values.value.error = 'Введите корректное значение';
-    return false
-  } else{
-    values.value.error=''
-  }
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${values.value.city}&appid=d4cea6e448f300abb46acd7e5a624672&units=metric&lang=ru`)
-    .then((res: { data: null; }) => (values.value.info = res.data))
+export async function getWeather(event: MouseEvent) {
+  event.preventDefault();
+  // if (values.city.trim().length < 3) {
+  //   values.error = 'Введите корректное значение';
+  //   return false
+  // } else{
+  //   values.error=''
+  // }
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=d4cea6e448f300abb46acd7e5a624672&units=metric&lang=ru`)
+  data.info = response.data
+  console.log(data.info);
 }
