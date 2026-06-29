49671764bd8263487370da1b609fcfd70ab2f4f6
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-29 17:52:36 +0600

    rename project for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/weather/',
+  base: '/weather-app/',
   server: {
     open: true
   }
